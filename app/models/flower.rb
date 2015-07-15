3b84ad4d585f92405746a0bcf6ca71f2512cd056
Max Tran <max.thien.tran@gmail.com>
2015-07-14 23:12:59 -0700

    validation added

--- app/models/flower.rb
+++ app/models/flower.rb
@@ -1,2 +1,9 @@
 class Flower < ActiveRecord::Base
+    validates :name, :description, :image_url, presence:true
+    validates :price, numericality: {greater_than_or_equal_to: 0.01}
+    validates :name, uniqueness: true
+    validates :image_url, allow_blank: true, format: {
+        with: %r{\.(gif | jpg | png)\Z}i,
+        message: 'must be GIF, JPG or PNG image.'
+    }
 end
