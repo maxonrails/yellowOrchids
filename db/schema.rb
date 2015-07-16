d4cfc22a66f949807307efafb75c17e1bf3c1770
Max Tran <max.thien.tran@gmail.com>
2015-07-16 01:23:16 -0700

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150715183222) do
+ActiveRecord::Schema.define(version: 20150716082215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,11 +36,23 @@ ActiveRecord::Schema.define(version: 20150715183222) do
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
     t.integer  "quantity",   default: 1
+    t.integer  "order_id"
   end
 
   add_index "line_items", ["cart_id"], name: "index_line_items_on_cart_id", using: :btree
   add_index "line_items", ["flower_id"], name: "index_line_items_on_flower_id", using: :btree
+  add_index "line_items", ["order_id"], name: "index_line_items_on_order_id", using: :btree
+
+  create_table "orders", force: :cascade do |t|
+    t.string   "name"
+    t.text     "address"
+    t.string   "email"
+    t.string   "pay_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   add_foreign_key "line_items", "carts"
   add_foreign_key "line_items", "flowers"
+  add_foreign_key "line_items", "orders"
 end
