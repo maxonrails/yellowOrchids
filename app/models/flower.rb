1d209ad0b4288bded7f3fafc13d924c9b9399b3e
Max Tran <max.thien.tran@gmail.com>
2015-07-16 01:39:51 -0700

    checkout button added

--- app/models/flower.rb
+++ app/models/flower.rb
@@ -1,6 +1,8 @@
 class Flower < ActiveRecord::Base
     has_many :line_items
+    has_many :orders, through: :line_items
     before_destroy :ensure_not_referenced_by_any_line_item
+
     validates :name, :description, :image_url, presence:true
     validates :price, numericality: {greater_than_or_equal_to: 0.01}
     validates :name, uniqueness: true
