095c8f887ea0c14922c39aa139f85f805f0dcbf6
Max Tran <max.thien.tran@gmail.com>
2015-07-17 00:36:10 -0700

    more seeding and bootstraping

--- app/models/flower.rb
+++ app/models/flower.rb
@@ -6,11 +6,11 @@ class Flower < ActiveRecord::Base
     validates :name, :description, :image_url, presence:true
     validates :price, numericality: {greater_than_or_equal_to: 0.01}
     validates :name, uniqueness: true
-    validates :image_url, allow_blank: true, format: {
-        with: %r{\.(gif | jpg | png)\Z}i,
-        message: 'must be GIF, JPG or PNG image.'
-    }
-    validates :name, length: {minimum: 10}
+    # validates :image_url, allow_blank: true, format: {
+    #     with: %r{\.(gif | jpg | png)\Z}i,
+    #     message: 'must be GIF, JPG or PNG image.'
+    # }
+    validates :name, length: {minimum: 5}
 
     def self.latest
         Flower.order(:updated_at).last
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ Flower.create!(name: "Bright and Beautiful", description: "Fancy lilies, roses, 
     flowers combine for the perfect combination in this popular
     vased bouquet that's just right for almost any expression of love,
     appreciation, or caring. Approximately 16 to 19 inches in height.",
-    image_url: 'brightspot.jpg',
+    image_url: "brightspot.jpg",
     price: 39.69)
 
 Flower.create!(name: "Heat Wave", description: "Send your warmest wishes with this colorful
@@ -19,20 +19,34 @@ Flower.create!(name: "Heat Wave", description: "Send your warmest wishes with th
     and orange tones. This vased arrangement includes Gerbera daisies,
     miniature roses, button poms, monte casino, solidaster, or similar fresh flower favorites.
     Great for a birthday, get well, thanks, or thinking of you.",
-    image_url: 'moreroses.jpg',
+    image_url: "HeatWave.jpg",
     price: 37.69)
 
 Flower.create!(name: "Sunny Sentiments", description: "Send a bouquet of instant happiness
     with this cheery combination of fresh roses, daisies, alstroemeria, and more in sunny yellow
     and white tones. Designed with fresh foliage in a quality glass vase. An uplighting gift for any
     occasion! Approximately 16 to 20 inches in height.",
-    image_url: 'SunnySentiments.jpg',
+    image_url: "SunnySentiments.jpg",
     price: 49.99)
 
 Flower.create!(name: "Wondorous Wishes", description: "Knock someone’s socks off with this colorful
     bouquet of fresh lilies, roses, hydrangea, Gerberas, alstroemeria, daisy poms, or similar fresh
     seasonal favorites designed in a stylish glass vase. Wonderful! Approximately 17 inches high
     x 14 inches wide.",
-    image_url: 'wondorous.jpg',
+    image_url: "wondorous.jpg",
     price: 39.69)
+
+Flower.create!(name: "White Roses Bouquet", description: "One dozen beautiful long-stem white roses
+    tastefully arranged with fresh foliage in a quality glass vase. Simply elegant!",
+    image_url: "white_roses.jpg",
+    price: 39.69)
+
+Flower.create!(name: "True Romance", description: "Send a message of love with style and flair.
+    This gorgeous bouquet features one dozen beautiful long stemmed roses in red and pink hues
+    for an unbeatable combination that says, 'You are Really Special!'
+        Approximately 22 inches H x 14 inches W as shown. ",
+    image_url: "true_romance.jpg",
+    price: 39.69)
+
+
 
